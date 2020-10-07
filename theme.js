55ca37304349288328b8d2a29fb4fc60158b1811
Olha Titova <o.titova.ua@gmail.com>
2020-10-07 15:47:14 +0300

    create site

---
+++ theme.js
@@ -0,0 +1,32 @@
+'use strict';
+
+
+let btn = document.querySelector('.theme-button');
+let link = document.querySelector('#theme-link');
+
+btn.addEventListener('click', () => { changeTheme() });
+
+function changeTheme() {
+    let orangeTheme = './css/theme.css';
+    let noTheme = '';
+
+    let currTheme = link.getAttribute('href');
+    let theme = '';
+
+    if (currTheme === orangeTheme) {
+        currTheme = noTheme;
+        theme = '';
+    } else {
+        currTheme = orangeTheme;
+        theme = 'theme';
+    }
+
+    link.setAttribute('href', currTheme);
+
+    // save(theme);
+    currentTheme = localStorage.getItem('theme');
+
+
+}
+
+
