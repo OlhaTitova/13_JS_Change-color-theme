f4f91e69cad40d8e2e65349fa4fdb8d2a0775ee5
Olha Titova <o.titova.ua@gmail.com>
2020-10-09 00:31:04 +0300

    create site

--- theme.js
+++ theme.js
@@ -4,9 +4,7 @@ document.body.onload = () => {
 
     document.documentElement.setAttribute('theme', localStorage.getItem('theme'));
 
-
     const toggleBtn = document.querySelector('#toggle-theme');
-    console.log(toggleBtn);
 
     toggleBtn.addEventListener('click', e => {
 
